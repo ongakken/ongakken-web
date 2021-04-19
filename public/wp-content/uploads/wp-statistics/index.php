cc13c467cf847a1542dfacaf88340bd627f892a9
Simon Slamka <51794014+simonSlamka@users.noreply.github.com>
2021-04-19 16:04:26 +0200

    [RISKY] removed the js dir from the neumoprhic theme;

--- public/wp-content/uploads/wp-statistics/index.php
+++
@@ -1,8 +0,0 @@
-<br /><br />
-<center>
-Sorry, you are not authorized to access this area ! Goodbye !
-</center>
-<br /><br />
-<center>
-Protect your online content with <a href="https://www.tychesoftwares.com/premium-plugins/">WP Content Copy Protection Pro</a> - The No.1 WordPress Copy Protection Plugin on the Web
-</center>
